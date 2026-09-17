3a966d35b01c110ce202409569c0556cd450c57e
Matias <45922889+saitamOsirb@users.noreply.github.com>
2026-09-17 17:34:43 -0300

    feat: add front-tile interaction service

---
+++ src/game/interaction/InteractionService.ts
@@ -0,0 +1,15 @@
+import { DIRECTION_VECTOR, type Direction } from '../constants'
+import type { GridPoint } from '../world/types'
+import { getNpcsForScene } from './npcs'
+import type { InteractableNpcDefinition } from './types'
+
+export class InteractionService {
+  facingTile(origin: Readonly<GridPoint>, direction: Direction): GridPoint {
+    const vector = DIRECTION_VECTOR[direction]
+    return { x: origin.x + vector.x, y: origin.y + vector.y }
+  }
+
+  findNpc(scenePath: string | null, tile: Readonly<GridPoint>): InteractableNpcDefinition | undefined {
+    return getNpcsForScene(scenePath).find((npc) => npc.tile.x === tile.x && npc.tile.y === tile.y)
+  }
+}

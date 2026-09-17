7311186ba691b40650c891ef6e70fbcf8ec5e2c5
Matias <45922889+saitamOsirb@users.noreply.github.com>
2026-09-17 01:06:30 -0300

    feat: add world data contracts

---
+++ src/game/world/types.ts
@@ -0,0 +1,39 @@
+import type { Direction } from '../constants'
+
+export interface GridPoint {
+  x: number
+  y: number
+}
+
+export interface DoorDefinition {
+  tile: GridPoint
+  nextScene: string
+  spawnTile: GridPoint
+  spawnDirection: Direction
+}
+
+export interface WorldObjectDefinition {
+  name: string
+  instancePath?: string
+  texturePath?: string
+  position: GridPoint
+  zIndex?: number
+}
+
+export interface TileDefinition {
+  x: number
+  y: number
+  tileId: number
+  autotileX: number
+  autotileY: number
+  flipX: boolean
+  flipY: boolean
+  transpose: boolean
+}
+
+export interface ImportedSceneDefinition {
+  name: string
+  tiles: TileDefinition[]
+  objects: WorldObjectDefinition[]
+  doors: DoorDefinition[]
+}
